6e930a91925b7e20098a2dbe6e2d3d06e7526999
Goosebay111 <george.p.lee.iii@gmail.com>
2023-01-21 19:51:00 +1000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -140,11 +140,16 @@ class CollectionBloc extends Bloc<CollectionEvents, CollectionState> {
 on<AddToNode>(
       (event, emit) {
         final List<CollectionState> nodes = state.getAllNodes(state);
+/// Expose the parent node.
         final CollectionState parent = nodes[event.index];
+/// add to the children of the parent, which changes the nested state of the data.
         parent.children.add(event.child);
-
+/// adds the updated nested data to the empty children list of the copyWith object.
+        //emit(state.copyWith(children: []..addAll(state.children)));
+        /// OR
         emit(state.copyWith(children: [...state.children]));
-        
+        /// OR
+        /// emit(state.copyWith(children: [...list[0].children]));
       },
     );
   }
